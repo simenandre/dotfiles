b694db7d427dcc6146143330a0b701055c77ee61
Simen A. W. Olsen <cobraz@cobraz.no>
2020-11-20 22:49:23 +0100

    fix: path issue

--- .vscode/vscode.sh
+++ .vscode/vscode.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 # Install VSCode extensions
-cat extensions.list | grep -v '^#' | xargs -L1 code --install-extension
+cat $DIR/extensions.list | grep -v '^#' | xargs -L1 code --install-extension
