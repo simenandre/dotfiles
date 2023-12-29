a90dcf3b1a919369328954be5221644955517254
Simen A. W. Olsen <so@bjerk.io>
2023-12-29 01:10:02 +0100

    remove vscode

--- .vscode/vscode.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-# Install VSCode extensions
-cat $DIR/extensions.list | grep -v '^#' | xargs -L1 code --install-extension
